3ee3029af8c50d7fde4e14eb5d8aff5c8162a00e
NGUYENHUYVU-DE180288 <vunhde18088@fpt.edu.vn>
2025-06-28 21:40:40 +0700

    Implement authentication support with middleware improvements and JWT validation. Adjust layout for conditional user actions.

--- Tourify-Frontend/Program.cs
+++ Tourify-Frontend/Program.cs
@@ -2,30 +2,34 @@ using Tourify_Frontend.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
 app.UseRouting();
 
-app.UseAuthorization();
 
-// Add our custom middleware
 app.UseMiddleware<AuthMiddleware>();
 
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// 6. Map routes
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
