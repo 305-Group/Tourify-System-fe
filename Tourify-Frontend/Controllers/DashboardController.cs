f4ec4e7e3bb4a2ed159fad8aa1cbae51d666fa00
khoa.nguyen-kim <khoashyofme24092004@gmail.com>
2025-06-28 20:51:14 +0700

    dashboard-screen (sub-company)

---
+++ Tourify-Frontend/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tourify_Frontend.Controllers
+{
+    // Nếu chỉ cho Sub-Company mới xem được, thêm [Authorize(Roles="SubCompany")]
+    [Authorize(Roles = "sub-company")]
+    public class DashboardController : Controller
+    {
+        // GET: /Dashboard
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
